48ee34c56b3b4cff0377a51e3017787d203d3c82
Bhavik Parmar <bhavik2936@gmail.com>
2021-03-25 22:21:42 +0530

    user deactive after game over
    User will be deactivated after finishing game and won't able to play again from same user (Have to create new user)

--- app/javascript/packs/questions.js
+++ app/javascript/packs/questions.js
@@ -6,20 +6,23 @@ let gameOver = false;
 let userScore = 0;
 let userStrike = 0;
 
+// on document ready, the current function will be get executed
 jQuery(function() {
+  // the list of questions will be fetched
   $.ajax({
     url: questionListUrl,
   }).done(function(response) {
 
     questionList = response;
     loadFirstQuestion();
-  }).fail(function(jqXHR, textstatus, errorThrown) {
+  }).fail(function(jqXHR, textStatus, errorThrown) {
 
     alert("Your request cannot be proceed at this time, Please try again later!");
   });
 
+  // user inputs are assessed and respective states are updated
   $('button').on('click', function(e) {
-    
+
     if (e.currentTarget.id === "ude" && questionList[questionPointer]['answer'] === true) {
       updateScore();
     } else if (e.currentTarget.id === "na-ude" && questionList[questionPointer]['answer'] === false) {
@@ -64,8 +67,15 @@ function updateStrike() {
   }
 }
 
+// The ScoreBoard will get saved for the same user and will be deactivated to prevent further activities
 function finishGame() {
+  insertScoreBoard();
+}
+
+// The ScoreBoard will get saved for the same user
+function insertScoreBoard() {
   newScoreBoardUrl = "/users/" + getUserId() + "/score_boards";
+
   userScoreBoard = {
     score_board: {
       user_id: getUserId(),
@@ -79,13 +89,27 @@ function finishGame() {
     url: newScoreBoardUrl,
     beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
     data: userScoreBoard
-  }).done(function(response) {
 
+  }).done(function(response) {
+    deactivateUser();
+  }).fail(function(jqXHR, textStatus, errorThrown) {
+    alert("Couldn't save score, Please try again later!");
+  });
+}
+
+// The User will get deactivated after its ScoreBoard been saved
+function deactivateUser(url) {
+  deactivateUserUrl = "/users/" + getUserId();
+
+  $.ajax({
+    type: "PATCH",
+    url: deactivateUserUrl,
+    beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+
+  }).done(function(response) {
     alert("Thanks for playing! Your Score: " + userScoreBoard.score_board.score);
     window.location.href = "/";
-  }).fail(function(jqXHR, textstatus, errorThrown) {
-
-    alert("Score cannot be saved at this moment!");
+  }).fail(function (jqXHR, textStatus, errorThrown) {
+    alert("User lock couldn't be completed!");
   });
-
 }
